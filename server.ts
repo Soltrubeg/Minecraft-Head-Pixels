2b4218bdbbe329817b9511ab9656f0e75a53f98f
Tqlted <benelsen2812@gmail.com>
2025-09-18 19:56:54 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -69,7 +69,11 @@ Deno.serve(async (req) => {
     const url = new URL(req.url);
     const uuid = url.searchParams.get("uuid");
     if (!uuid) {
-      return new Response(JSON.stringify({status: 400, statusText: "Missing UUID" }));
+      return new Response(JSON.stringify({status: 400, statusText: "Missing UUID" }), {
+        headers: { "Content-Type": "application/json" },
+        status: 400,
+        statusText: "Missing UUID"
+      });
     }
     const colors = await getFaceColors(uuid);
     return new Response(JSON.stringify(colors), {
